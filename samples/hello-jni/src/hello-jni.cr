c311e5b10856a2f0e12f43aefa5affa26c86ec15
Julien Portalier <julien@portalier.com>
2016-11-25 15:45:21 +0100

    Sample: hello-jni (Java activity calls Crystal)

---
+++ samples/hello-jni/src/hello-jni.cr
@@ -0,0 +1,5 @@
+require "../../../src/jni"
+
+fun helloJni_stringFromJNI = Java_com_example_hellojni_HelloJni_stringFromJNI(env : JNI::Env, this : JNI::JClass) : JNI::JString
+  env.value.functions.value.newStringUTF.call(env, "Hello from JNI !".to_unsafe)
+end
