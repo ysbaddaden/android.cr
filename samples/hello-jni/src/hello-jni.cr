42b41b3beb516da2b335aface12443b81791b5c9
Julien Portalier <julien@portalier.com>
2016-12-14 15:08:36 +0100

    Sample: hello-jni (call Crystal functions from Java)
    Not really interesting in terms of behavior, and probably broken if
    called from different threads, unless the called functions are very
    limited to a few blocking operations and never do any IO or
    something that may trigger fiber switches.

--- samples/hello-jni/src/hello-jni.cr
+++ samples/hello-jni/src/hello-jni.cr
@@ -1,4 +1,4 @@
-require "../../../src/jni"
+require "../../../src/jni/onload"
 
 fun helloJni_stringFromJNI = Java_com_example_hellojni_HelloJni_stringFromJNI(env : JNI::Env, this : JNI::JClass) : JNI::JString
   env.value.functions.value.newStringUTF.call(env, "Hello from JNI !".to_unsafe)
